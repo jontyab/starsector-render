8a2de7373956e32c9c128212234bb94c17d52b97
jontyab <jontyab@users.noreply.github.com>
2026-03-21 20:59:26 +0000

    Code cleanup, Linux combat fixes, upstream javaagent support

--- modules/renderer/src/com/genir/renderer/loaders/ModTransformAgent.java
+++ modules/renderer/src/com/genir/renderer/loaders/ModTransformAgent.java
@@ -3,23 +3,9 @@ package com.genir.renderer.loaders;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
-import java.util.Arrays;
-import java.util.List;
 
 /** Java agent that intercepts mod class loading to route GL calls through FR bridges. */
 public class ModTransformAgent {
-    private static final List<ClassConstantTransformer> modGLTransformers = List.of(
-            new ClassConstantTransformer(Arrays.asList(
-                    ClassConstantTransformer.newTransform("org/lwjgl/opengl/GL11", "com/genir/renderer/bridge/GL11"),
-                    ClassConstantTransformer.newTransform("org/lwjgl/opengl/GL13", "com/genir/renderer/bridge/GL13"),
-                    ClassConstantTransformer.newTransform("org/lwjgl/opengl/GL14", "com/genir/renderer/bridge/GL14"),
-                    ClassConstantTransformer.newTransform("org/lwjgl/opengl/GL20", "com/genir/renderer/bridge/GL20"),
-                    ClassConstantTransformer.newTransform("org/lwjgl/opengl/GL30", "com/genir/renderer/bridge/GL30"),
-                    ClassConstantTransformer.newTransform("org/lwjgl/opengl/GL43", "com/genir/renderer/bridge/GL43"),
-                    ClassConstantTransformer.newTransform("org/lwjgl/opengl/GLContext", "com/genir/renderer/bridge/GLContext"),
-                    ClassConstantTransformer.newTransform("org/lwjgl/opengl/ARBFramebufferObject", "com/genir/renderer/bridge/GL30")
-            ))
-    );
 
     public static void premain(String args, Instrumentation inst) {
         inst.addTransformer(new ClassFileTransformer() {
@@ -27,7 +13,7 @@ public class ModTransformAgent {
             public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
                                     ProtectionDomain protectionDomain, byte[] classfileBuffer) {
                 if (className != null && className.startsWith("org/dark/")) {
-                    return ClassTransformer.transformBytes(className + ".class", classfileBuffer, modGLTransformers);
+                    return ClassTransformer.transformBytes(className + ".class", classfileBuffer, ClassConstantTransformer.MOD_GL_TRANSFORMS);
                 }
                 return null;
             }
