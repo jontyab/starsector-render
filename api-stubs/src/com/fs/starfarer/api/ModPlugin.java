51477a808c5f8395dad458b0f8c6ddc3211d0600
jontyab <jontyab@users.noreply.github.com>
2026-05-31 19:20:31 +0100

    macOS port: platform mappings, GraphicsLib agent, build tooling

---
+++ api-stubs/src/com/fs/starfarer/api/ModPlugin.java
@@ -0,0 +1,6 @@
+package com.fs.starfarer.api;
+
+/** Mod runtime plugin hook. Compile-time stub for starfarer.api.jar. */
+public interface ModPlugin {
+    void onApplicationLoad();
+}
---
+++ api-stubs/src/com/fs/starfarer/api/impl/campaign/procgen/MarkovNames.java
@@ -0,0 +1,7 @@
+package com.fs.starfarer.api.impl.campaign.procgen;
+
+/** Procedural name cache init. Compile-time stub for starfarer.api.jar. */
+public final class MarkovNames {
+    public static void loadIfNeeded() {
+    }
+}
