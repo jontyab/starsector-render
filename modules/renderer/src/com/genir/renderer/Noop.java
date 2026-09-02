5e96cf95364d8152f7f941597ec7d2c987946b49
genir <45612481+Halke1986@users.noreply.github.com>
2026-09-02 15:12:16 +0200

    noop caller

--- modules/renderer/src/com/genir/renderer/Noop.java
+++ modules/renderer/src/com/genir/renderer/Noop.java
@@ -1,9 +1,17 @@
 package com.genir.renderer;
 
 public class Noop {
+    public static void caller() {
+        noop();
+        noop(null);
+    }
+
+    // invokestatic Method com/genir/renderer/Noop noop ()V
     public static void noop() {
     }
 
+    // aload_0
+    // invokestatic Method com/genir/renderer/Noop noop (Ljava/lang/String;)V
     public static void noop(String var0) {
     }
 }
