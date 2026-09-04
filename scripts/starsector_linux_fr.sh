8fed3908785be17a5fc8b3f6bd98c10b0a293cf2
jontyab <jontyab@users.noreply.github.com>
2026-09-04 23:50:05 +0100

    CI: bundle Zulu 27 EA JRE for Linux x64

--- scripts/starsector_linux_fr.sh
+++ scripts/starsector_linux_fr.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-JAVA=./jre_fr/bin/java
-[ -x "$JAVA" ] || JAVA=./jre_linux/bin/java
+JAVA=./jre_linux/bin/java
 
 "$JAVA" \
     -Dfile.encoding=UTF-8 \
-	-noverify \
 	-XX:+UnlockDiagnosticVMOptions \
 	-XX:+UnlockExperimentalVMOptions \
 	-XX:+ShowCodeDetailsInExceptionMessages \
